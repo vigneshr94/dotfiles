403997e3262e70f29b722c35a7af7a65de8be873
R Vigneswar <vigneshr.1894@gmail.com>
2024-04-12 22:15:52 +0530

    added new starship prompt and tmux conf and alises

--- zsh_custom_setup/plugins/aliases.plugin.zsh
+++ zsh_custom_setup/plugins/aliases.plugin.zsh
@@ -3,18 +3,18 @@ alias vi=nvim
 alias vim=nvim
 alias cat=bat
 alias ls=colorls
-alias tf="tmux ls | fzf --border --height 40% --reverse | cut -d ':' -f 1 | xargs -n 1 tmux switchc -t"
+alias tf="tmux ls | fzf-tmux --border --height 40% --reverse | cut -d ':' -f 1 | xargs -n 1 tmux switchc -t"
 alias plz=sudo
 alias lg=lazygit
-alias ffwp='find * -type f | fzf --border --reverse --preview="bat --number --color=always {}"'
-alias fgc="git log --oneline | fzf --reverse --border --multi --preview 'git show {+1}'"
-alias ffwop='find * -type f | fzf --border --reverse --preview-window hidden --bind "?:preview:bat --color=always --number {}"'
+alias ffwp='find * -type f | fzf-tmux --border --reverse --preview="bat --number --color=always {}"'
+alias fgc="git log --oneline | fzf-tmux --reverse --border --multi --preview 'git show {+1}'"
+alias ffwop='find * -type f | fzf-tmux --border --reverse --preview-window hidden --bind "?:preview:bat --color=always --number {}"'
 alias omzr="omz reload"
 
 find_and_edit ()
 {
   local file
-  file=($(find * -type f | fzf --border --reverse --preview="bat --number --color=always {}"))
+  file=($(find * -type f | fzf-tmux --border --reverse --preview="bat --number --color=always {}"))
   if [ "$file" != "" ]; then
     vim $file
   fi
@@ -27,7 +27,7 @@ find_and_edit_with_editor_of_choice ()
     local file
     local editor=$1
     if [ -x "$(command -v "$editor")" ]; then
-        file=$(find * -type f | fzf --border --reverse --preview="bat --number --color=always {}")
+        file=$(find * -type f | fzf-tmux --border --reverse --preview="bat --number --color=always {}")
         if [ "$file" != "" ] && [ "$editor" != "" ]; then
             $editor $file
         fi
